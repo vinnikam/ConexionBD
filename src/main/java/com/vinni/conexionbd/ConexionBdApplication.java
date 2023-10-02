cad81887b3754c6c759ff9a745397dbf7c3bd9e6
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2023-10-02 17:23:48 -0500

    Initial commit

---
+++ src/main/java/com/vinni/conexionbd/ConexionBdApplication.java
@@ -0,0 +1,13 @@
+package com.vinni.conexionbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexionBdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConexionBdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinni/conexionbd/ConexionBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinni.conexionbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexionBdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
